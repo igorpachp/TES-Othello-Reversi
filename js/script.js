943e495698bcbf20afe936adc717ae62570593ed
igorpachp <igorpacheco9@gmail.com>
2022-10-19 22:26:18 -0300

    adicionada função para construir o tabuleiro

---
+++ js/script.js
@@ -0,0 +1,32 @@
+DEFAULT_BOARD_SIZE = 8;
+
+const board = document.getElementById('board');
+
+let size = DEFAULT_BOARD_SIZE;
+createBoard(size+2);
+
+function createBoard(size) {
+    for (let i = 0; i < size; i++) {
+        let row;
+
+        row = document.createElement('div');
+        row.classList.add('row');
+        if (i == 0 || i == size - 1) {
+            row.classList.add('hidden');
+        }
+        board.appendChild(row);
+    }
+    
+    const rows = board.getElementsByClassName('row');
+    for (let row of rows) {
+        for (let j = 0; j < size; j++) {
+            let cell = createCell();
+
+            if (j == 0 || j == size - 1 || row == board.firstChild || row == board.lastChild) {
+                cell.classList.add('hidden');
+            }
+            cells.push(cell);
+            row.appendChild(cell);
+        }
+    }
+}
